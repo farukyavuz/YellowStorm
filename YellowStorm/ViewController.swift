a3333d1b7b96fbcd7eec3839d0acbfaea4254025
Faruk Yavuz <faruk@email.com>
2018-03-17 15:45:34 +0300

    Initial Commit

---
+++ YellowStorm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YellowStorm
+//
+//  Created by Faruk Yavuz on 17.03.2018.
+//  Copyright © 2018 Faruk Yavuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
